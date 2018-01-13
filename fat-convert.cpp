f255a9d562eac26560652d89e554f1b443d51577
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-01-13 13:26:42 +0100

    Move fat structs and functions to fat.h, use memmap instead of reading fs

--- fat-convert.cpp
+++ fat-convert.cpp
@@ -5,8 +5,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#include "fat.h"
+#include "partition.h"
+
 uint32_t END_OF_CHAIN = 0xFFFFFF8;
 TAILQ_HEAD(listhead, extent_lentry);
+
+struct boot_sector boot_sector;
+struct meta_info meta_info;
 
 struct extent {
 	uint32_t logical_start;  // First file cluster number that this extent covers
@@ -18,62 +24,6 @@ struct extent_lentry {
 	TAILQ_ENTRY(extent_lentry) entries;
 	struct extent extent;
 };
-
-struct fat_dentry {
-	uint8_t short_name[8];
-	uint8_t short_extension[3];
-	uint8_t attrs;
-	uint8_t long_name_case;
-	uint8_t create_time_10_ms;
-	uint16_t create_time;
-	uint16_t create_date;
-	uint16_t access_date;
-	uint16_t first_cluster_high;
-	uint16_t mod_time;
-	uint16_t mod_date;
-	uint16_t first_cluster_low;
-	uint32_t file_size;
-};
-
-struct __attribute__((packed)) boot_sector {
-	uint8_t jump_instruction[3];
-	uint8_t oem_name[8];
-	uint16_t bytes_per_sector;
-	uint8_t sectors_per_cluster;
-	uint16_t sectors_before_fat;
-	uint8_t fat_count;
-	uint16_t dir_entries;
-	uint16_t sector_count;
-	uint8_t media_descriptor;
-	uint16_t unused2;
-	uint16_t sectors_per_disk_track;
-	uint16_t disk_heads;
-	uint32_t hidden_sectors_before_partition;
-	uint32_t total_sectors2;  // used if total_sectors would have overflown;
-	uint32_t sectors_per_fat;
-	uint16_t drive_description_flags;
-	uint16_t version;
-	uint32_t root_cluster_no;
-	uint16_t fs_info_sector_no;
-	uint16_t backup_boot_sector_no;
-	uint8_t reserved[12];
-	uint8_t physical_drive_no;
-	uint8_t reserved2;
-	uint8_t ext_boot_signature;
-	uint32_t volume_id;
-	uint8_t volume_label[11];
-	uint64_t fs_type;
-};
-
-struct meta_info {
-	char *fat_start;
-	uint16_t fat_entries;
-	uint32_t cluster_size;
-	char *data_start;
-};
-
-struct meta_info MetaInfo;
-struct boot_sector BootSector;
 
 bool is_dir(struct fat_dentry *dentry) {
 	return dentry->attrs & 0x10;
@@ -95,23 +45,12 @@ bool is_dot_dir(struct fat_dentry *dentry) {
 	return dentry->short_name[0] == '.';
 }
 
-uint64_t fat_entry(uint32_t cluster_no) {
-	return MetaInfo.fat_start + cluster_no * 4;
-}
-
-uint64_t cluster_start(uint32_t cluster_no) {
-	return MetaInfo.data_start + (cluster_no - 2) * MetaInfo.cluster_size;
-}
-
-struct listhead read_extents(uint32_t cluster_no, FILE *fs) {
+struct listhead read_extents(uint32_t cluster_no) {
 	struct listhead head;
 	TAILQ_INIT(&head);
 	struct extent current_extent = {0, 1, cluster_no};
 
-	char buf[4];
-	fseek(fs, fat_entry(cluster_no), SEEK_SET);
-	fread(buf, 1, 4, fs);
-	uint32_t next_cluster_no = *(uint32_t*) buf;
+	uint32_t next_cluster_no = *(uint32_t*) fat_entry(cluster_no);
 
 	while (next_cluster_no < END_OF_CHAIN) {
 		if (next_cluster_no == current_extent.physical_start + current_extent.length) {
@@ -123,10 +62,8 @@ struct listhead read_extents(uint32_t cluster_no, FILE *fs) {
 
 			struct extent new_extent = {current_extent.logical_start + current_extent.length, 1, next_cluster_no};
 			current_extent = new_extent;
-			fseek(fs, fat_entry(next_cluster_no), SEEK_SET);
 		}
-		fread(buf, 1, 4, fs);
-		next_cluster_no = *(uint32_t*) buf;
+		next_cluster_no = *(uint32_t*) fat_entry(next_cluster_no);
 	}
 	struct extent_lentry *new_lentry = (struct extent_lentry *) malloc(sizeof(struct extent_lentry));
 	new_lentry->extent = current_extent;
@@ -134,24 +71,25 @@ struct listhead read_extents(uint32_t cluster_no, FILE *fs) {
 	return head;
 }
 
-char *read_dir(struct listhead extents_list, FILE *fs) {
+uint8_t *read_dir(struct listhead extents_list) {
 	size_t dir_size = 0;
 	for (struct extent_lentry *it = extents_list.tqh_first; it != NULL; it = it->entries.tqe_next) {
 		dir_size += it->extent.length;
 	}
 
-	char *dir_data = (char*) malloc(dir_size * MetaInfo.cluster_size);
+	uint8_t *dir_data = (uint8_t*) malloc(dir_size * meta_info.cluster_size);
 	for (struct extent_lentry *it = extents_list.tqh_first; it != NULL; it = it->entries.tqe_next) {
 		struct extent current_extent = it->extent;
-		fseek(fs, cluster_start(current_extent.physical_start), SEEK_SET);
-		fread(dir_data + current_extent.logical_start, MetaInfo.cluster_size, current_extent.length, fs);
+		memcpy(dir_data + current_extent.logical_start,
+		       cluster_start(current_extent.physical_start),
+		       meta_info.cluster_size * current_extent.length);
 	}
 	return dir_data;
 }
 
-void recursive_traverse(uint32_t cluster_no, FILE *fs) {
-	struct listhead root_extents = read_extents(cluster_no, fs);
-	char *root_data = read_dir(root_extents, fs);
+void recursive_traverse(uint32_t cluster_no) {
+	struct listhead root_extents = read_extents(cluster_no);
+	uint8_t *root_data = read_dir(root_extents);
 
 	struct fat_dentry *current_dentry = (struct fat_dentry *) root_data;
 	for (; !is_dir_table_end(current_dentry); current_dentry++) {
@@ -162,7 +100,7 @@ void recursive_traverse(uint32_t cluster_no, FILE *fs) {
 					uint32_t high = current_dentry->first_cluster_high << 16;
 					uint32_t dir_cluster_no = high | low;
 					printf("dir: %.8s\n", current_dentry->short_name);
-					recursive_traverse(dir_cluster_no, fs);
+					recursive_traverse(dir_cluster_no);
 				}
 			} else {
 				printf("file: %.8s\n", current_dentry->short_name);
@@ -171,15 +109,16 @@ void recursive_traverse(uint32_t cluster_no, FILE *fs) {
 	}
 }
 
-void read_boot_sector(FILE *fs) {
-	fread(&BootSector, sizeof(struct boot_sector), 1, fs);
+void read_boot_sector(uint8_t *fs) {
+	boot_sector = *(struct boot_sector*) fs;
 }
 
-void set_meta_info() {
-	MetaInfo.fat_start = BootSector.sectors_before_fat * BootSector.bytes_per_sector;
-	MetaInfo.fat_entries = BootSector.sectors_per_fat / BootSector.sectors_per_cluster;
-	MetaInfo.cluster_size = BootSector.sectors_per_cluster * BootSector.bytes_per_sector;
-	MetaInfo.data_start = MetaInfo.fat_start + BootSector.fat_count * BootSector.sectors_per_fat * BootSector.bytes_per_sector;
+void set_meta_info(uint8_t *fs) {
+	meta_info.fs_start = fs;
+	meta_info.fat_start = fs + boot_sector.sectors_before_fat * boot_sector.bytes_per_sector;
+	meta_info.fat_entries = boot_sector.sectors_per_fat / boot_sector.sectors_per_cluster;
+	meta_info.cluster_size = boot_sector.sectors_per_cluster * boot_sector.bytes_per_sector;
+	meta_info.data_start = meta_info.fat_start + boot_sector.fat_count * boot_sector.sectors_per_fat * boot_sector.bytes_per_sector;
 }
 
 int main(int argc, char **argv) {
@@ -187,8 +126,9 @@ int main(int argc, char **argv) {
 		printf("Wrong usage\n");
 		exit(1);
 	}
-	FILE *fs = fopen(argv[1], "r");
-	read_boot_sector(fs);
-	set_meta_info();
-	recursive_traverse(BootSector.root_cluster_no, fs);
+	Partition partition = {.path = argv[1]};
+	openPartition(&partition);
+	read_boot_sector(partition.ptr);
+	set_meta_info(partition.ptr);
+	recursive_traverse(boot_sector.root_cluster_no);
 }
--- fat.h
+++ fat.h
@@ -1,17 +1,74 @@
 #ifndef OFS_CONVERT_FAT_H
 #define OFS_CONVERT_FAT_H
 
+#include <stdint.h>
 
 constexpr uint32_t CLUSTER_ENTRY_MASK = 0x0FFFFFFF;
 constexpr uint32_t FREE_CLUSTER = 0;
 
+extern struct boot_sector boot_sector;
+extern struct meta_info meta_info;
 
-struct meta_info {
-    char* fat_start;
-    uint16_t fat_entries;
-    uint32_t cluster_size;
-    char* data_start;
+struct __attribute__((packed)) boot_sector {
+    uint8_t jump_instruction[3];
+    uint8_t oem_name[8];
+    uint16_t bytes_per_sector;
+    uint8_t sectors_per_cluster;
+    uint16_t sectors_before_fat;
+    uint8_t fat_count;
+    uint16_t dir_entries;
+    uint16_t sector_count;
+    uint8_t media_descriptor;
+    uint16_t unused2;
+    uint16_t sectors_per_disk_track;
+    uint16_t disk_heads;
+    uint32_t hidden_sectors_before_partition;
+    uint32_t total_sectors2;  // used if total_sectors would have overflown;
+    uint32_t sectors_per_fat;
+    uint16_t drive_description_flags;
+    uint16_t version;
+    uint32_t root_cluster_no;
+    uint16_t fs_info_sector_no;
+    uint16_t backup_boot_sector_no;
+    uint8_t reserved[12];
+    uint8_t physical_drive_no;
+    uint8_t reserved2;
+    uint8_t ext_boot_signature;
+    uint32_t volume_id;
+    uint8_t volume_label[11];
+    uint64_t fs_type;
 };
 
+struct meta_info {
+    uint8_t* fs_start;
+    uint8_t* fat_start;
+    uint16_t fat_entries;
+    uint32_t cluster_size;
+    uint8_t* data_start;
+};
+
+struct fat_dentry {
+    uint8_t short_name[8];
+    uint8_t short_extension[3];
+    uint8_t attrs;
+    uint8_t long_name_case;
+    uint8_t create_time_10_ms;
+    uint16_t create_time;
+    uint16_t create_date;
+    uint16_t access_date;
+    uint16_t first_cluster_high;
+    uint16_t mod_time;
+    uint16_t mod_date;
+    uint16_t first_cluster_low;
+    uint32_t file_size;
+};
+
+uint8_t *fat_entry(uint32_t cluster_no) {
+	return meta_info.fat_start + cluster_no * 4;
+}
+
+uint8_t *cluster_start(uint32_t cluster_no) {
+	return meta_info.data_start + (cluster_no - 2) * meta_info.cluster_size;
+}
 
 #endif //OFS_CONVERT_FAT_H
