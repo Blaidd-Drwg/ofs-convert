af0d1ec771986dad3963a850a0f4967389e74451
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-01-14 23:29:05 +0100

    Refactor fat.cpp

--- fat.cpp
+++ fat.cpp
@@ -8,7 +8,7 @@
 #include "fat.h"
 #include "partition.h"
 
-TAILQ_HEAD(listhead, extent_lentry);
+TAILQ_HEAD(extent_list, extent_lentry);
 
 struct boot_sector boot_sector;
 struct meta_info meta_info;
@@ -30,6 +30,12 @@ uint8_t *fat_entry(uint32_t cluster_no) {
 
 uint8_t *cluster_start(uint32_t cluster_no) {
 	return meta_info.data_start + (cluster_no - FAT_START_INDEX) * meta_info.cluster_size;
+}
+
+uint32_t file_cluster_no(struct fat_dentry *dentry) {
+	uint16_t low = dentry->first_cluster_low;
+	uint32_t high = dentry->first_cluster_high << 16;
+	return high | low;
 }
 
 bool is_dir(struct fat_dentry *dentry) {
@@ -68,8 +74,8 @@ uint8_t lfn_entry_sequence_no(struct fat_dentry *dentry) {
 	return *(uint8_t *) dentry & 0x1F;
 }
 
-struct listhead read_extents(uint32_t cluster_no) {
-	struct listhead head;
+struct extent_list read_extents(uint32_t cluster_no) {
+	struct extent_list head;
 	TAILQ_INIT(&head);
 	struct extent current_extent = {0, 1, cluster_no};
 
@@ -79,7 +85,7 @@ struct listhead read_extents(uint32_t cluster_no) {
 		if (next_cluster_no == current_extent.physical_start + current_extent.length) {
 			current_extent.length++;
 		} else {
-			struct extent_lentry *new_lentry = (struct extent_lentry *) malloc(sizeof(struct extent_lentry));
+			struct extent_lentry *new_lentry = (struct extent_lentry *) malloc(sizeof *new_lentry);
 			new_lentry->extent = current_extent;
 			TAILQ_INSERT_TAIL(&head, new_lentry, entries);
 
@@ -88,20 +94,22 @@ struct listhead read_extents(uint32_t cluster_no) {
 		}
 		next_cluster_no = *(uint32_t*) fat_entry(next_cluster_no);
 	}
-	struct extent_lentry *new_lentry = (struct extent_lentry *) malloc(sizeof(struct extent_lentry));
+	struct extent_lentry *new_lentry = (struct extent_lentry *) malloc(sizeof *new_lentry);
 	new_lentry->extent = current_extent;
 	TAILQ_INSERT_TAIL(&head, new_lentry, entries);
 	return head;
 }
 
-uint8_t *read_dir(struct listhead extents_list) {
+uint8_t *read_dir(uint32_t cluster_no) {
+	struct extent_list extents = read_extents(cluster_no);
+
 	size_t dir_size = 0;
-	for (struct extent_lentry *it = extents_list.tqh_first; it != NULL; it = it->entries.tqe_next) {
+	for (struct extent_lentry *it = extents.tqh_first; it != NULL; it = it->entries.tqe_next) {
 		dir_size += it->extent.length;
 	}
 
 	uint8_t *dir_data = (uint8_t*) malloc(dir_size * meta_info.cluster_size);
-	for (struct extent_lentry *it = extents_list.tqh_first; it != NULL; it = it->entries.tqe_next) {
+	for (struct extent_lentry *it = extents.tqh_first; it != NULL; it = it->entries.tqe_next) {
 		struct extent current_extent = it->extent;
 		memcpy(dir_data + current_extent.logical_start,
 		       cluster_start(current_extent.physical_start),
@@ -110,34 +118,36 @@ uint8_t *read_dir(struct listhead extents_list) {
 	return dir_data;
 }
 
+uint16_t *parse_long_name(struct fat_dentry *dentry, uint16_t *long_name) {
+	uint8_t lfn_sequence_no = lfn_entry_sequence_no(dentry);
+	if (is_last_lfn_entry(dentry)) {
+		int max_name_length = LFN_ENTRY_LENGTH * lfn_sequence_no + 1;
+		long_name = (uint16_t *) malloc(max_name_length * sizeof(uint16_t));
+		long_name[max_name_length - 1] = 0;
+	}
+	lfn_cpy(long_name, dentry, lfn_sequence_no);
+	return long_name;
+}
+
 void recursive_traverse(uint32_t cluster_no, uint16_t *long_name) {
-	struct listhead dir_extents = read_extents(cluster_no);
-	uint8_t *dir_data = read_dir(dir_extents);
+	uint8_t *dir_data = read_dir(cluster_no);
 
 	struct fat_dentry *current_dentry = (struct fat_dentry *) dir_data;
 	for (; !is_dir_table_end(current_dentry); current_dentry++) {
 		if (!is_invalid(current_dentry)) {
 			if (is_lfn(current_dentry)) {
-				uint8_t lfn_sequence_no = lfn_entry_sequence_no(current_dentry);
-				if (is_last_lfn_entry(current_dentry)) {
-					int max_name_length = LFN_ENTRY_LENGTH * lfn_sequence_no + 1;
-					long_name = (uint16_t *) malloc(max_name_length * sizeof(uint16_t));
-					long_name[max_name_length - 1] = 0;
-				}
-				lfn_cpy(long_name, current_dentry, lfn_sequence_no);
+				long_name = parse_long_name(current_dentry, long_name);
 			} else if (is_dir(current_dentry)) {
 				if (!is_dot_dir(current_dentry)) {
-					uint16_t low = current_dentry->first_cluster_low;
-					uint32_t high = current_dentry->first_cluster_high << 16;
-					uint32_t dir_cluster_no = high | low;
 					printf("dir: %.8s\n", current_dentry->short_name);
-					recursive_traverse(dir_cluster_no, long_name);
+					recursive_traverse(file_cluster_no(current_dentry), long_name);
 				}
 			} else {
 				printf("file: %.8s\n", current_dentry->short_name);
 			}
 		}
 	}
+	free(dir_data);
 }
 
 void read_boot_sector(uint8_t *fs) {
@@ -150,20 +160,4 @@ void set_meta_info(uint8_t *fs) {
 	meta_info.fat_entries = boot_sector.sectors_per_fat / boot_sector.sectors_per_cluster;
 	meta_info.cluster_size = boot_sector.sectors_per_cluster * boot_sector.bytes_per_sector;
 	meta_info.data_start = meta_info.fat_start + boot_sector.fat_count * boot_sector.sectors_per_fat * boot_sector.bytes_per_sector;
-}
-
-int main(int argc, char **argv) {
-	if (argc < 2) {
-		printf("Wrong usage\n");
-		exit(1);
-	}
-	Partition partition = {.path = argv[1]};
-	if (!openPartition(&partition)) {
-		fprintf(stderr, "Failed to open partition");
-		return 1;
-	}
-
-	read_boot_sector(partition.ptr);
-	set_meta_info(partition.ptr);
-	recursive_traverse(boot_sector.root_cluster_no, NULL);
 }
