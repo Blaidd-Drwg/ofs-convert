4d5cec6455f81245daf66570fdbbd480b08658a4
Alexander Meißner <AlexanderMeissner@gmx.net>
2018-01-20 14:02:05 +0100

    Added binary search for blocked extends Allocator is a singleton now

--- extent-allocator.cpp
+++ extent-allocator.cpp
@@ -1,31 +1,45 @@
 #include "extent-allocator.h"
 #include <stdlib.h>
 
-bool can_be_used(extent_allocator& allocator) {
-    if(allocator.index_in_fat < allocator.blocked_extents->physical_start)
-        return is_free_cluster(meta_info.fat_start[allocator.index_in_fat++]);
-    allocator.index_in_fat = allocator.blocked_extents->physical_start + allocator.blocked_extents->length;
-    ++allocator.blocked_extents;
-    return false;
-}
+extent_allocator allocator;
 
 int extent_sort_compare(const void* eA, const void* eB) {
     return reinterpret_cast<const extent*>(eA)->physical_start
          - reinterpret_cast<const extent*>(eB)->physical_start;
 }
 
-void init_extent_allocator(extent_allocator& allocator) {
+void init_extent_allocator() {
     allocator.index_in_fat = FAT_START_INDEX;
+    allocator.blocked_extent_current = allocator.blocked_extents;
     qsort(allocator.blocked_extents, allocator.blocked_extent_count, sizeof(extent), extent_sort_compare);
-    while(!can_be_used(allocator));
 }
 
-extent allocate_extent(extent_allocator& allocator, uint32_t& remaining_count) {
+bool can_be_used() {
+    if(allocator.index_in_fat < allocator.blocked_extent_current->physical_start)
+        return is_free_cluster(meta_info.fat_start[allocator.index_in_fat++]);
+    allocator.index_in_fat = allocator.blocked_extent_current->physical_start + allocator.blocked_extent_current->length;
+    ++allocator.blocked_extent_current;
+    return false;
+}
+
+extent allocate_extent(uint32_t& remaining_count) {
+    while(!can_be_used());
     extent result;
     result.physical_start = allocator.index_in_fat;
-    while(can_be_used(allocator));
+    while(can_be_used());
     result.length = allocator.index_in_fat - result.physical_start;
     remaining_count -= result.length;
-    while(!can_be_used(allocator));
     return result;
+}
+
+uint32_t find_blocked_extents(uint32_t physical_start) {
+    uint32_t begin = 0, mid, end = allocator.blocked_extent_count;
+    while(begin < end) {
+        mid = (begin+end)/2;
+        if(allocator.blocked_extents[mid].physical_start < physical_start)
+            begin = mid+1;
+        else
+            end = mid;
+    }
+    return begin;
 }
