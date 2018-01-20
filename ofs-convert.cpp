738eec5ee3fc5229bf75bdf22efd57341bd2226e
Alexander Meißner <AlexanderMeissner@gmx.net>
2018-01-20 15:52:06 +0100

    Added init_extent_allocator() call Removed struct qualifiers

--- ofs-convert.cpp
+++ ofs-convert.cpp
@@ -1,6 +1,7 @@
 #include "fat.h"
 #include "partition.h"
 #include "stream-archiver.h"
+#include "extent-allocator.h"
 
 #include <ctype.h>
 #include <stdio.h>
@@ -10,30 +11,30 @@
 extern uint64_t pageSize;
 
 struct cluster_read_state {
-    struct extent current_extent;
+    extent* current_extent;
     uint32_t extent_cluster;
-    struct fat_dentry *current_cluster;
+    fat_dentry* current_cluster;
     uint32_t cluster_dentry;
 };
 
-struct fat_dentry *next_dentry(StreamArchiver *extent_stream, struct cluster_read_state *state) {
-    struct fat_dentry *ret;
+fat_dentry* next_dentry(StreamArchiver* extent_stream, cluster_read_state* state) {
+    fat_dentry* ret;
     do {
         state->cluster_dentry++;
         if (state->cluster_dentry < meta_info.dentries_per_cluster) {
             ret = state->current_cluster + state->cluster_dentry;
         } else {
             state->extent_cluster++;
-            if (state->extent_cluster < state->current_extent.length) {
-                uint32_t cluster_no = state->current_extent.physical_start + state->extent_cluster;
-                state->current_cluster = (struct fat_dentry *) cluster_start(cluster_no);
+            if (state->extent_cluster < state->current_extent->length) {
+                uint32_t cluster_no = state->current_extent->physical_start + state->extent_cluster;
+                state->current_cluster = reinterpret_cast<fat_dentry*>(cluster_start(cluster_no));
                 state->cluster_dentry = 0;
                 ret = state->current_cluster;
             } else {
-                state->current_extent = *(struct extent *) iterateStreamArchiver(extent_stream, false, sizeof(struct extent));
+                state->current_extent = reinterpret_cast<extent*>(iterateStreamArchiver(extent_stream, false, sizeof(extent)));
                 state->extent_cluster = 0;
-                uint32_t cluster_no = state->current_extent.physical_start + state->extent_cluster;
-                state->current_cluster = (struct fat_dentry *) cluster_start(cluster_no);
+                uint32_t cluster_no = state->current_extent->physical_start + state->extent_cluster;
+                state->current_cluster = reinterpret_cast<fat_dentry*>(cluster_start(cluster_no));
                 state->cluster_dentry = 0;
                 ret = state->current_cluster;
             }
@@ -42,42 +43,42 @@ struct fat_dentry *next_dentry(StreamArchiver *extent_stream, struct cluster_rea
     return ret;
 }
 
-struct fat_dentry *init_cluster_read_state(StreamArchiver *extent_stream, struct cluster_read_state *state) {
-    state->current_extent = *(struct extent *) iterateStreamArchiver(extent_stream, false, sizeof(struct extent));
+fat_dentry* init_cluster_read_state(StreamArchiver* extent_stream, cluster_read_state* state) {
+    state->current_extent = reinterpret_cast<extent*>(iterateStreamArchiver(extent_stream, false, sizeof(extent)));
     state->extent_cluster = 0;
-    uint32_t cluster_no = state->current_extent.physical_start + state->extent_cluster;
-    state->current_cluster = (struct fat_dentry *) cluster_start(cluster_no);
+    uint32_t cluster_no = state->current_extent->physical_start + state->extent_cluster;
+    state->current_cluster = reinterpret_cast<fat_dentry*>(cluster_start(cluster_no));
     state->cluster_dentry = -1;  // dummy value that next_dentry will increment to 0
     return next_dentry(extent_stream, state);
 }
 
-void reserve_name(uint16_t *pointers[], int count, StreamArchiver *write_stream) {
+void reserve_name(uint16_t* pointers[], int count, StreamArchiver* write_stream) {
     for (int i = 0; i < count; i++) {
-        pointers[i] = (uint16_t *) iterateStreamArchiver(write_stream, true, LFN_ENTRY_LENGTH * sizeof(uint16_t));
+        pointers[i] = reinterpret_cast<uint16_t*>(iterateStreamArchiver(write_stream, true, LFN_ENTRY_LENGTH * sizeof(uint16_t)));
     }
     cutStreamArchiver(write_stream);
 }
 
-struct fat_dentry *reserve_dentry(StreamArchiver *write_stream) {
-    void *p = iterateStreamArchiver(write_stream, true, sizeof(struct fat_dentry));
+fat_dentry* reserve_dentry(StreamArchiver* write_stream) {
+    void* ptr = iterateStreamArchiver(write_stream, true, sizeof(fat_dentry));
     cutStreamArchiver(write_stream);
-    return (struct fat_dentry *) p;
+    return reinterpret_cast<fat_dentry*>(ptr);
 }
 
-struct extent *reserve_extent(StreamArchiver *write_stream) {
-    void *p = iterateStreamArchiver(write_stream, true, sizeof(struct extent));
+extent* reserve_extent(StreamArchiver* write_stream) {
+    void* ptr = iterateStreamArchiver(write_stream, true, sizeof(extent));
     cutStreamArchiver(write_stream);
-    return (struct extent *) p;
+    return reinterpret_cast<extent*>(ptr);
 }
 
-uint32_t *reserve_children_count(StreamArchiver *write_stream) {
-    void *p = iterateStreamArchiver(write_stream, true, sizeof(uint32_t));
+uint32_t* reserve_children_count(StreamArchiver* write_stream) {
+    void* ptr = iterateStreamArchiver(write_stream, true, sizeof(uint32_t));
     cutStreamArchiver(write_stream);
-    return (uint32_t *) p;
+    return reinterpret_cast<uint32_t*>(ptr);
 }
 
-void read_extents(uint32_t cluster_no, StreamArchiver *write_stream) {
-    struct extent *current_extent = reserve_extent(write_stream);
+void read_extents(uint32_t cluster_no, StreamArchiver* write_stream) {
+    extent *current_extent = reserve_extent(write_stream);
     *current_extent = {0, 1, cluster_no};
 
     uint32_t next_cluster_no = *fat_entry(cluster_no);
@@ -86,7 +87,7 @@ void read_extents(uint32_t cluster_no, StreamArchiver *write_stream) {
         if (next_cluster_no == current_extent->physical_start + current_extent->length) {
             current_extent->length++;
         } else {
-            struct extent new_extent = {current_extent->logical_start + current_extent->length, 1, next_cluster_no};
+            extent new_extent = {current_extent->logical_start + current_extent->length, 1, next_cluster_no};
             current_extent = reserve_extent(write_stream);
             *current_extent = new_extent;
         }
@@ -94,35 +95,35 @@ void read_extents(uint32_t cluster_no, StreamArchiver *write_stream) {
     }
 }
 
-struct fat_dentry *read_lfn(struct fat_dentry *first_entry, StreamArchiver *extent_stream, uint16_t *name[], int lfn_entry_count, struct cluster_read_state *state) {
-    uint8_t *entry = (uint8_t *) first_entry;
+fat_dentry* read_lfn(fat_dentry* first_entry, StreamArchiver* extent_stream, uint16_t* name[], int lfn_entry_count, struct cluster_read_state* state) {
+    uint8_t* entry = reinterpret_cast<uint8_t*>(first_entry);
     for (int i = lfn_entry_count - 1; i >= 0; i--) {
         lfn_cpy(name[i], entry);
-        entry = (uint8_t *) next_dentry(extent_stream, state);
+        entry = reinterpret_cast<uint8_t*>(next_dentry(extent_stream, state));
     }
-    return (struct fat_dentry *) entry;
+    return reinterpret_cast<fat_dentry*>(entry);
 }
 
-void traverse(StreamArchiver *dir_extent_stream, StreamArchiver *write_stream) {
-    struct cluster_read_state state = {};
+void traverse(StreamArchiver* dir_extent_stream, StreamArchiver* write_stream) {
+    cluster_read_state state = {};
 
-    uint32_t *children_count = reserve_children_count(write_stream);
-    struct fat_dentry *current_dentry = init_cluster_read_state(dir_extent_stream, &state);
+    uint32_t* children_count = reserve_children_count(write_stream);
+    fat_dentry* current_dentry = init_cluster_read_state(dir_extent_stream, &state);
 
     while (!is_dir_table_end(current_dentry)) {
         bool has_long_name = is_lfn(current_dentry);
         if (has_long_name) {
             int lfn_entry_count = lfn_entry_sequence_no(current_dentry);
-            uint16_t *name[lfn_entry_count];
+            uint16_t* name[lfn_entry_count];
             reserve_name(name, lfn_entry_count, write_stream);
             current_dentry = read_lfn(current_dentry, dir_extent_stream, name, lfn_entry_count, &state);
         } else {
-            uint16_t *name[1];
+            uint16_t* name[1];
             reserve_name(name, 1, write_stream);
             read_short_name(current_dentry, name[0]);
         }
 
-        struct fat_dentry *dentry = reserve_dentry(write_stream);
+        fat_dentry* dentry = reserve_dentry(write_stream);
         memcpy(dentry, current_dentry, sizeof *current_dentry);
 
         uint32_t cluster_no = file_cluster_no(current_dentry);
@@ -139,13 +140,13 @@ void traverse(StreamArchiver *dir_extent_stream, StreamArchiver *write_stream) {
     }
 }
 
-void init_stream_archiver(StreamArchiver *stream) {
+void init_stream_archiver(StreamArchiver* stream) {
     pageSize = 32;
     memset(stream, 0, sizeof *stream);
     cutStreamArchiver(stream);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, const char** argv) {
     if (argc < 2) {
         printf("Wrong usage\n");
         exit(1);
@@ -158,6 +159,7 @@ int main(int argc, char **argv) {
 
     read_boot_sector(partition.ptr);
     set_meta_info(partition.ptr);
+    init_extent_allocator();
 
     StreamArchiver stream;
     init_stream_archiver(&stream);
