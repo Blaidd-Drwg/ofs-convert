74d7862d905ff830c61683f1afbfcbf18fbf9c25
Alexander Meißner <AlexanderMeissner@gmx.net>
2018-01-30 21:24:46 +0100

    Correct bug fix

--- metadata_reader.cpp
+++ metadata_reader.cpp
@@ -119,15 +119,15 @@ void find_blocked_extent_fragments(const fat_extent& input_extent, StreamArchive
 }
 
 void aggregate_extents(uint32_t cluster_no, StreamArchiver* write_stream) {
-    fat_extent current_extent {0, 1, cluster_no};
+    fat_extent current_extent {0, 0, cluster_no};
     while(true) {
         bool is_end = cluster_no >= FAT_END_OF_CHAIN,
-             is_consecutive = cluster_no == current_extent.physical_start + current_extent.length - 1,
+             is_consecutive = cluster_no == current_extent.physical_start + current_extent.length,
              has_max_length = current_extent.length == UINT16_MAX;
         if(is_end || !is_consecutive || has_max_length) {
             find_blocked_extent_fragments(current_extent, write_stream);
             current_extent.logical_start += current_extent.length;
-            current_extent.length = 1;
+            current_extent.length = 0;
             current_extent.physical_start = cluster_no;
         } else
             ++current_extent.length;
