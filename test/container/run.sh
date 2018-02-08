0f0f42a8bc8e2edf6ef20cc8dfe15d57f71e4723
David Stangl <david.stangl@student.hpi.de>
2018-02-08 18:50:04 +0100

    Make cmake build type configurable for container

--- test/container/run.sh
+++ test/container/run.sh
@@ -6,4 +6,5 @@ docker run --rm \
            -v $(realpath ./build):/build \
            -v $(realpath ..):/test \
            -e OFS_CONVERT_TOOL_TIMEOUT="$OFS_CONVERT_TOOL_TIMEOUT" \
+           -e CMAKE_CONFIGURATION="$CMAKE_CONFIGURATION" \
            ofs-convert-testing
