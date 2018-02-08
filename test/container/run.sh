ce88c6f83ea828701562f5f915c86ee4e015a824
David Stangl <david.stangl@student.hpi.de>
2018-02-08 18:39:22 +0100

    Passthrough tool timeout to docker container

--- test/container/run.sh
+++ test/container/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 mkdir -p build
-docker run --rm --privileged -v $(realpath ../..):/src:ro -v $(realpath ./build):/build -v $(realpath ..):/test ofs-convert-testing
+docker run --rm \
+           --privileged \
+           -v $(realpath ../..):/src:ro \
+           -v $(realpath ./build):/build \
+           -v $(realpath ..):/test \
+           -e OFS_CONVERT_TOOL_TIMEOUT="$OFS_CONVERT_TOOL_TIMEOUT" \
+           ofs-convert-testing
